2ee9d8ee085bdb6b8e4c8207cbdd66307828cb0f
Brian Johnson <ssbkbrice@gmail.com>
2022-12-05 00:45:52 -0600

    i hope you work

--- myAzureFunctionsProject01/index.js
+++ myAzureFunctionsProject01/index.js
@@ -5,14 +5,10 @@ module.exports = async function (context, req) {
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-    if(name == "Eric"){
-        const newMessage = name
-            ? " Welcome to the secret club" + name
-            :"do you see what i see?";
-    }
+
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage,newMessage
+        body: responseMessage
     };
 }
