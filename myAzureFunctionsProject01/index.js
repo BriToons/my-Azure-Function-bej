37c4117d135819c63391b3b0bb5e6462ba7cf377
Brian Johnson <ssbkbrice@gmail.com>
2022-12-05 02:10:33 -0600

    made some function

--- myAzureFunctionsProject01/index.js
+++ myAzureFunctionsProject01/index.js
@@ -2,10 +2,15 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    const lastname = (req.query.lastname || (req.body && req.body.lastname));
 
+    responseMessage = "Welcome to the site \nWho might you be?"
+    if (name != undefined){responseMessage = "Hello " + name + " hope youre having a wonderful day!"}
+    if (name === "Eric" || name === "Brian"){responseMessage = "Welcome to the secret side of the site " + name + "\n"}
+    if(lastname != undefined && name != undefined){
+        responseMessage = "Oh so youre " + name + " " + lastname + ". Why didnt you just say so?" 
+        if(name == "Eric" && lastname == "Pogue"){responseMessage = "Hello Professor, you entered the backrooms of the site.\nOnly entering your first and last name gets you here"}
+    }
 
     context.res = {
         // status: 200, /* Defaults to 200 */
